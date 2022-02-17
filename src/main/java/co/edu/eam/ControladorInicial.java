acd73e27ff451bc9cdbb30728e597f666f6ad261
Carlos Tovar <kharlozm@gmail.com>
2022-02-17 15:57:31 -0500

    Sesión uno 14 de febrero. Primer proyecto con Spring Boot.

--- src/main/java/co/edu/eam/ControladorInicial.java
+++ src/main/java/co/edu/eam/ControladorInicial.java
@@ -1,18 +1,44 @@
 package co.edu.eam;
 
+import co.edu.eam.domain.Persona;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @Controller
 @Slf4j
 public class ControladorInicial {
 
+    @Value("${index.despedida}")
+    private String despedida;
+
     @GetMapping("/")
-    public String inicio(){
-        log.info("ejecutando el método inicio de la clase Controlador Inicial");
-        log.debug("Haciendo debug desde el método inicio de la clase Controlador Inicial");
+    public String inicio(Model model){
+        var saludo = "Hola de nuevo";
+        model.addAttribute("saludo", saludo);
+        model.addAttribute("despedida", despedida);
+
+        var persona1 = new Persona();
+        persona1.setNombres("Carlos");
+        persona1.setApellidos("Tovar");
+        persona1.setProfesion("Ing. de Sistemas");
+        persona1.setEmail("carlos@eam.edu.co");
+
+        var persona2 = new Persona();
+        persona2.setNombres("Juan");
+        persona2.setApellidos("Pérez");
+        persona2.setProfesion("Desarrollador");
+        persona2.setEmail("mauricio@eam.edu.co");
+
+        var personas = Arrays.asList(persona1, persona2);
+
+        model.addAttribute("personas", personas);
+
         return "index";
     }
 
---
+++ src/main/java/co/edu/eam/domain/Persona.java
@@ -0,0 +1,11 @@
+package co.edu.eam.domain;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+    private String nombres;
+    private String apellidos;
+    private String profesion;
+    private String email;
+}
