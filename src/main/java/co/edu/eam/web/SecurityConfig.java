748d33a81f6d0b5652f686c13d5b9d620451f712
Carlos Tovar <kharlozm@gmail.com>
2022-03-03 15:59:35 -0500

    Sesión seis 3 de marzo. Validaciones y seguridad. Parte 2.

---
+++ src/main/java/co/edu/eam/web/SecurityConfig.java
@@ -0,0 +1,33 @@
+package co.edu.eam.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin1234")
+                .roles("ADMIN", "USER")
+                .and()
+                .withUser("usuario")
+                .password("usuario1234")
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/registrar/**","/editar/**", "/eliminar/**")
+                .hasRole("ADMIN")
+                .antMatchers("/")
+                .hasAnyRole("USER", "ADMIN");
+    }
+}
