3f276283a7bca27aa77b4e1dfab459b1594efea4
Carlos Tovar <kharlozm@gmail.com>
2022-03-31 16:06:40 -0500

    Sesión doce 31 de marzo. Error con SecurityConfig Bean Referencia circular.

--- src/main/java/co/edu/eam/service/UsuarioService.java
+++ src/main/java/co/edu/eam/service/UsuarioService.java
@@ -3,22 +3,27 @@ package co.edu.eam.service;
 import co.edu.eam.dao.UsuarioDao;
 import co.edu.eam.domain.Rol;
 import co.edu.eam.domain.Usuario;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
 @Service("userDetailsService")
+@Slf4j
 public class UsuarioService implements UserDetailsService {
     @Autowired
     private UsuarioDao usuarioDao;
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Usuario usuario = usuarioDao.findByUsername(username);
 
@@ -31,7 +36,7 @@ public class UsuarioService implements UserDetailsService {
         for(Rol rol: usuario.getRoles()){
             roles.add(new SimpleGrantedAuthority(rol.getNombre()));
         }
-        // todo completar 28/03/2022
-        //return
+
+        return new User(usuario.getUsername(), usuario.getPassword(), roles);
     }
 }
--- src/main/java/co/edu/eam/web/SecurityConfig.java
+++ src/main/java/co/edu/eam/web/SecurityConfig.java
@@ -1,25 +1,30 @@
 package co.edu.eam.web;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("{noop}admin1234")
-                .roles("ADMIN", "USER")
-                .and()
-                .withUser("usuario")
-                .password("{noop}usuario1234")
-                .roles("USER");
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder pE(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configurerGlobal (AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(pE());
     }
 
     @Override
